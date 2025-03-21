40455f49b7d46fb98b0bef61a9cb9bf08538986e
geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-03-21 05:40:04 +0000

    commit

---
+++ Practice/MySystem.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MySystem {
+
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int x = sc.nextInt();
+         int y = sc.nextInt();
+
+        System.out.println("This is for printing the writing characters.");
+        System.err.println("This is how we throw an error with System.err");
+   System.out.printf("Addition: %d" , x+y);
+    }
+}
